7bb3a1f648a84637ae390b723b024215a1f5dab6
José Quintino <josequintinno@icloud.com>
2024-07-02 16:52:20 -0300

    :::... INDIVIDUUMAPI ...:::

---
+++ src/main/java/br/com/quintinno/INDIVIDUUMAPI/IndividuumapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.quintinno.INDIVIDUUMAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividuumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividuumapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/quintinno/INDIVIDUUMAPI/IndividuumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quintinno.INDIVIDUUMAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividuumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
