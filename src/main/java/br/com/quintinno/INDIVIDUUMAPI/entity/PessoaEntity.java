894f7adc638ee7da3270f786deac255e50cc1f59
José Quintino <josequintinno@icloud.com>
2024-07-03 00:00:32 -0300

    🔹 feature: Conectar API ao banco de dados PostgreSQL

--- src/main/java/br/com/quintinno/INDIVIDUUMAPI/entity/PessoaEntity.java
+++ src/main/java/br/com/quintinno/INDIVIDUUMAPI/entity/PessoaEntity.java
@@ -1,11 +1,28 @@
 package br.com.quintinno.INDIVIDUUMAPI.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PESSOA")
 public class PessoaEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO", nullable = false)
     private Long codigo;
 
+    @ManyToOne
+    @JoinColumn(name = "ID_TIPO_PESSOA", nullable = false)
     private TipoPessoaEntity tipoPessoaEntity;
 
+    @Column(name = "NOME", nullable = false)
     private String nome;
 
     public PessoaEntity () { }
--- src/main/java/br/com/quintinno/INDIVIDUUMAPI/entity/TipoPessoaEntity.java
+++ src/main/java/br/com/quintinno/INDIVIDUUMAPI/entity/TipoPessoaEntity.java
@@ -1,9 +1,22 @@
 package br.com.quintinno.INDIVIDUUMAPI.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_TIPO_PESSOA")
 public class TipoPessoaEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO", nullable = false)
     private Long codigo;
 
+    @Column(name = "DESCRICAO", nullable = false)
     private String descricao;
 
     public TipoPessoaEntity() { }
