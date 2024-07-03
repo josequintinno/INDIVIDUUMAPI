db475ea753aa5a2cce4265994ef0afdd1fa8ba13
José Quintino <josequintinno@icloud.com>
2024-07-02 23:41:55 -0300

    🔹 feature: Implementar modelagem das classes de Entidade

--- src/main/java/br/com/quintinno/INDIVIDUUMAPI/entity/PessoaEntity.java
+++ src/main/java/br/com/quintinno/INDIVIDUUMAPI/entity/PessoaEntity.java
@@ -2,4 +2,36 @@ package br.com.quintinno.INDIVIDUUMAPI.entity;
 
 public class PessoaEntity {
 
+    private Long codigo;
+
+    private TipoPessoaEntity tipoPessoaEntity;
+
+    private String nome;
+
+    public PessoaEntity () { }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public TipoPessoaEntity getTipoPessoaEntity() {
+        return tipoPessoaEntity;
+    }
+
+    public void setTipoPessoaEntity(TipoPessoaEntity tipoPessoaEntity) {
+        this.tipoPessoaEntity = tipoPessoaEntity;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
