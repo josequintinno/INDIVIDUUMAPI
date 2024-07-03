3761e124006c5dc1768794c8ea49451b4f5c63e2
José Quintino <josequintinno@icloud.com>
2024-07-03 16:40:55 -0300

    🔹 feature: Implementar modelagem de banco de dados

--- src/main/resources/sql/individuumapi.sql
+++ src/main/resources/sql/individuumapi.sql
@@ -1,5 +1,7 @@
 drop table if exists tb_tipo_pessoa cascade;
 drop table if exists tb_pessoa cascade;
+drop table tb_tipo_documento cascade;
+drop table if exists tb_documento cascade;
 
 create table if not exists tb_tipo_pessoa (
 	codigo serial not null,
@@ -19,6 +21,24 @@ create table if not exists tb_pessoa (
 	constraint ck_pessoa_nome unique (nome)
 );
 
+create table if not exists tb_tipo_documento (
+	codigo serial not null,
+	descricao varchar not null,
+	sigla varchar not null,
+	constraint pk_tipo_documento primary key (codigo),
+	constraint ck_tipo_documento_descricao unique (descricao)
+);
+
+create table if not exists tb_documento (
+	codigo serial not null,
+	id_tipo_documento integer not null,
+	id_pessoa integer not null,
+	numero varchar not null,
+	orgao_emissor varchar not null,
+	constraint pk_documento primary key (codigo),
+	constraint fk_pessoa foreign key (id_pessoa) references tb_pessoa (codigo)
+);
+
 insert into tb_tipo_pessoa (descricao) values ('Pessoa Física');
 insert into tb_tipo_pessoa (descricao) values ('Pessoa Jurídica');
 insert into tb_tipo_pessoa (descricao) values ('Pessoa Autoridade');
@@ -30,9 +50,22 @@ insert into tb_pessoa (id_tipo_pessoa, nome, proprietario, e_ativo) values (
 true
 );
 
+insert into tb_tipo_documento (sigla, descricao) values ('RG', 'Registro Geral');
+insert into tb_tipo_documento (sigla, descricao) values ('CPF', 'Cadastro de Pessoa Física');
+insert into tb_tipo_documento (sigla, descricao) values ('CNPJ', 'Cadastro Nacional de Pessoa Jurídica');
+
+insert into tb_documento (id_pessoa, numero, orgao_emissor) values (
+	(select codigo from tb_pessoa where nome = 'José Quintinno'),
+	(select codigo from tb_tipo_documento where sigla = 'RG'),
+	'152783453',
+	'Secretaria de Seguranca Pública - DF'
+);
+
 /*
     select * from tb_tipo_pessoa;
     select * from tb_pessoa;
+	select * from tb_tipo_documento;
+	select * from tb_documento;
 */
 
 tb_pessoa ();
@@ -40,11 +73,3 @@ tb_pessoa ();
 tb_pessoa_fisica ();
 
 tb_pessoa_juridica ();
-
-create table if not exists tb_documento (
-	codigo serial not null,
-	id_pessoa integer not null,
-	numero varchar not null,
-	data_vencimento null,
-	orgao_emissor varchar not null,
-);
